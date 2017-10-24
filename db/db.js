572e9b081fdb661f7ecfb5e844315efe6f9b47b8
r-best <bobbyisbest3@gmail.com>
2017-10-23 20:26:21 -0400

    Setting up for 508 project

--- db/db.js
+++ db/db.js
@@ -1,49 +1,19 @@
 module.exports.getCampaigns = function getCampaigns(){
-    var db = global.collections;
-    return db.campaigns.find().then(res => {
-        return res;
-    });
+    
 }
 
 module.exports.getCampaign = function getCampaign(name){
-    var db = global.collections;
-    return db.campaigns.findOne({
-        name: name
-    }).then(res => {
-        return res;
-    });
+    
 }
 
 module.exports.getPlayersInCampaign = function getPlayersInCampaign(campaign){
-    var db = global.collections;
-    return db.campaigns.findOne({
-        name: campaign
-    }).then(res => {
-        return db.players.find({
-            campaign: res.id
-        }).then(res2 => {
-            return res2;
-        });
-    });
+    
 }
 
 module.exports.getPlayerInCampaign = function getPlayerInCampaign(campaign, name){
-    var db = global.collections;
-    return db.campaigns.findOne({
-        name: campaign
-    }).then(res => {
-        return db.players.findOne({
-            name: name,
-            campaign: res.id
-        }).then(res2 => {
-            return res2;
-        });
-    });
+    
 }
 
 module.exports.getSpells = function getSpells(){
-    var db = global.collections;
-    return db.spells.find().then(res => {
-        return res;
-    });
+    
 }
